1374ba8a796decfdcb65b1589a52f60c25069128
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-06-19 12:47:05 +0530

    Updated conditions

--- pwd_checker.py
+++ pwd_checker.py
@@ -6,36 +6,35 @@ def pwd_strength(pwd):
         score += 1
     else:
         feedback += f"\n-> Password is short. ({len(pwd)} characters)."
+
+    if any(char.isupper() for char in pwd):
+        score += 1
+    else:
+        feedback += f"\n-> Missing uppercase letters."
     
-    for char in pwd:
-        if any(char.isupper()):
-            score += 1
-        else:
-            feedback += f"\n-> Missing uppercase letters."
-        
-        if any(char.islower()):
-            score += 1
-        else:
-            feedback += f"\n-> Missing lowercase letters."
-        
-        if any(char.isdigit()):
-            score += 1
-        else:
-            feedback += f"\n-> Missing numbers."
-        
-        symbols = "!@#$%^&*()-_+=|{[}]:;'<,>.?/"
-        if any(char in symbols):
-            score += 1
-        else:
-            feedback += f"\n-> Missing special characters or symbols."
-        
-        if score >= 5:
-            strength = "Strong"
-        elif score == 4:
-            strength = "Medium"
-        elif score == 3:
-            strength = "Weak"
-        else:
-            strength = "Very Weak"
+    if any(char.islower() for char in pwd):
+        score += 1
+    else:
+        feedback += f"\n-> Missing lowercase letters."
+    
+    if any(char.isdigit() for char in pwd):
+        score += 1
+    else:
+        feedback += f"\n-> Missing numbers."
+    
+    symbols = "!@#$%^&*()-_+=|{[}]:;'<,>.?/"
+    if any(char in symbols for char in pwd):
+        score += 1
+    else:
+        feedback += f"\n-> Missing special characters or symbols."
+    
+    if score >= 5:
+        strength = "Strong"
+    elif score == 4:
+        strength = "Medium"
+    elif score == 3:
+        strength = "Weak"
+    else:
+        strength = "Very Weak"
     
     return score, f"Password Strength: {strength}\n{feedback}."
