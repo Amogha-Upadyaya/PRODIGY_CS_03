0f1ce016b9985461b1679c4071a8e441be708245
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-06-19 12:42:36 +0530

    Updated PASSWORD GUIDELINES

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from pwd_checker import pwd_strength
 def main():
     while True:
         print("""PASSWORD GUIDELINES:
-              -> Length = Minimum 12 characters
-              -> Should include upper and lower case letters, numbers and special characters
-              -> Do not set a password already in use
-              -> Do not include personal information such as name, date of birth, etc.
+    -> Length = Minimum 12 characters
+    -> Should include upper and lower case letters, numbers and special characters
+    -> Do not set a password already in use
+    -> Do not include personal information such as name, date of birth, etc.
 
               """)
 
